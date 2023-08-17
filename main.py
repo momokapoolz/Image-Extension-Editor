702bf5b256912f48e25b5b01930578e7b1f8015d
momokapool <lgbao2004@gmail.com>
2023-08-17 17:22:41 +0700

    add upload file func in main python

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename   #uploading file method of Flask
 import cv2
 
+
+UPLOAD_FOLDER = 'upload'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__, template_folder='template')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
-def memaybeo():
+def starter():
     return render_template("webpage.html")
+
+
+@app.route("/edit", methods = ["GET", "POST"] )
+def edit():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
 
 app.run(debug= True)
 
